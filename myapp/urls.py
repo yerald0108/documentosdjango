0dc1785ea6812b34f53618a2e28c139cdf873ecd
yerald0108 <yeraldfuste@gmail.com>
2026-03-10 15:49:57 -0400

    home

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('formulario-f01/', views.formulario_f01, name='formulario_f01'),
+    path('formulario-f07/', views.formulario_f07, name='formulario_f07'),
     path('formulario-f12/', views.formulario_f12, name='formulario_f12'),
+    path('formulario-f42/', views.formulario_f42, name='formulario_f42'),
+    path('formulario-f60/', views.formulario_f60, name='formulario_f60'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'myapp/home.html')
+
+def formulario_f01(request):
+    return render(request, 'myapp/F.01_documento.html')
+
+
+def formulario_f07(request):
+    return render(request, 'myapp/F.07_documento.html')
+
 
 def formulario_f12(request):
-    return render(request, 'myapp/formulario_f12.html')
+    return render(request, 'myapp/F.12_documento.html')
+
+
+def formulario_f42(request):
+    return render(request, 'myapp/F.42documento.html')
+
+
+def formulario_f60(request):
+    return render(request, 'myapp/F.60_documento.html')
